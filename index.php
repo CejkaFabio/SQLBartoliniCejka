6860ddb8ef60b20f7b301ef7e1022ec9960f4e6a
CejkaFabio <realfmc7@gmail.com>
2018-02-15 14:37:34 +0100

    14:37

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username="root";
+$password="";
+$dbname="Utenti";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-?>
+
+// Create database
+$sql = "SELECT id, Nome, Cognome, Email FROM Credenziali";
+if ($conn->query($sql) === TRUE) {
+    while($row=$result->fetch_assoc()){
+    echo "<tr>";
+    echo "<td>".$row['id']."</td>";
+    echo "<td>".$row['Nome']."</td>";
+    echo "<td>".$row['Cognome']."</td>";
+    echo "<td><button type=\"button\" class=\"btn btn-primary\">Update</button></td>";
+    echo "<td><button type=\"button\" class=\"btn btn-danger\">Update</button></td>";
+    echo "</tr>";
+    }
+}
+
+$conn->close();
