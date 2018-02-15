2b2b33ce4e672986d1d624dca6bd3171a92bf466
bartosimo <bartosimo00@gmail.com>
2018-02-15 14:54:23 +0100

    14/54

--- index.php
+++ index.php
@@ -7,16 +7,17 @@ $dbname="Utenti";
 $conn = new mysqli($servername);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connessione Fallita: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, Nome, Cognome, Email FROM Credenziali";
+$sql = "SELECT id, Nome, Cognome, Email FROM Registro";
 if ($conn->query($sql) === TRUE) {
     while($row=$result->fetch_assoc()){
     echo "<tr>";
     echo "<td>".$row['id']."</td>";
     echo "<td>".$row['Nome']."</td>";
     echo "<td>".$row['Cognome']."</td>";
+    echo "<td>".$row['Email']."</td>";
     echo "<td><button type=\"button\" class=\"btn btn-primary\">Update</button></td>";
     echo "<td><button type=\"button\" class=\"btn btn-danger\">Update</button></td>";
     echo "</tr>";
