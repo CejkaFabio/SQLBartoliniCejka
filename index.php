fd0fd37a8fb23ea9ef40daa6ebe79a65b7eeefc7
CejkaFabio <realfmc7@gmail.com>
2018-02-16 09:36:22 +0100

    09:36

--- index.php
+++ index.php
@@ -2,25 +2,34 @@
 $servername = "localhost";
 $username="root";
 $password="";
-$dbname="Utenti";
+$dbname="registro";
 
-$conn = new mysqli($servername);
+
+$conn = new mysqli($servername,$username,$password,$dbname);
 
 if ($conn->connect_error) {
-    die("Connessione Fallita: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, Nome, Cognome, Email FROM Registro";
+$query = "INSERT INTO login (user, password) VALUES ('pippo', '$password')";
+
+// Esecuzione della query e controllo degli eventuali errori
+if (!$mysqli->query($query)) {
+    die($mysqli->error);
+}
+
+
+$sql = "SELECT id, Nome, Cognome, Email FROM Credenziali";
 if ($conn->query($sql) === TRUE) {
     while($row=$result->fetch_assoc()){
     echo "<tr>";
     echo "<td>".$row['id']."</td>";
     echo "<td>".$row['Nome']."</td>";
     echo "<td>".$row['Cognome']."</td>";
-    echo "<td>".$row['Email']."</td>";
     echo "<td><button type=\"button\" class=\"btn btn-primary\">Update</button></td>";
     echo "<td><button type=\"button\" class=\"btn btn-danger\">Update</button></td>";
     echo "</tr>";
     }
 }
+
 $conn->close();
