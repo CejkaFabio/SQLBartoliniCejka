c21b7a8c10118ba3c96a61a5d0caf3d20393bfde
CejkaFabio <realfmc7@gmail.com>
2018-02-20 15:42:27 +0100

    15:42

--- script/Cerca.js
+++ script/Cerca.js
@@ -1,8 +1,13 @@
 function Cerca(parola,elementi) {
 for(var i=0;i<elementi.length;i++)
 {
-    if(parola==elementi[i].innerHTML){
-        elementi[i].style.backgroundColor="lightblue";
+    elementi[i].style.backgroundColor="white";
+}
+for(var i=0;i<elementi.length;i++)
+{
+    if(elementi[i] == text.contains(parola);{
+   // if(parola==elementi[i].innerHTML) {
+        elementi[i].style.backgroundColor = "lightblue";
     }
 }
 }
