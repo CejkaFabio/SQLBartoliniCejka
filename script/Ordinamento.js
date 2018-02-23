58ecd265b2594d4b65ff491e445ea450a5b4821c
CejkaFabio <realfmc7@gmail.com>
2018-02-23 14:46:40 +0100

    14.46

--- script/Ordinamento.js
+++ script/Ordinamento.js
@@ -1,5 +1,6 @@
 function Ordinamento(parola,elementi)
 {
+    alert('ciao');
 var vettore=new Array();
 var vettore2=new Array();
 for(var i=0;i<elementi.length;i++)
@@ -8,7 +9,9 @@ for(var i=0;i<elementi.length;i++)
     vettore2[i]=parola[i+1].innerHTML;
 }
 vettore.sort();
+    alert('2ciao');
 for(var i=0;i<vettore.length;i++) {
 parola[i+1].innerHTML=vettore2[vettore[i][vettore[i].length-1]];
+
 }
 }
