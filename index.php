084bcf76adb1c6c300809165612e7d5c30a0e57c
bartosimo <bartosimo00@gmail.com>
2018-02-23 12:22:28 +0100

    12.22

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="script/Cerca.js"></script>
     <script type="text/javascript" src="script/Caricamento.js"></script>
+    <script type="text/javascript" src="script/Ordinamento.js"></script>
      <style>
         table, th, td {
             border: 1px solid black;
